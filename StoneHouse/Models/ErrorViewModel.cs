26a6cca74095bf7241d2489255ca1b4bf533d11f
Nazir <n_ahmad20@live.co.uk>
2019-05-26 12:39:23 +0100

    Add project files.

---
+++ StoneHouse/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace StoneHouse.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
