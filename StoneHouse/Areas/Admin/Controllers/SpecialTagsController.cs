036828d550850bae3f2a7ad284fb892cd21610fa
Nazir <n_ahmad20@live.co.uk>
2019-05-28 13:03:17 +0100

    Special Tag navigation

---
+++ StoneHouse/Areas/Admin/Controllers/SpecialTagsController.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using StoneHouse.Data;
+using StoneHouse.Models;
+
+namespace StoneHouse.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class SpecialTagsController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public SpecialTagsController(ApplicationDbContext db)
+            {
+                _db=db;
+            }
+
+
+        public IActionResult Index()
+        {
+            return View(_db.SpecialTags.ToList());
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create( SpecialTags specialTags)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Add(specialTags);
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+
+            }
+            else
+            {
+                return View(specialTags);
+            }
+
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id != null)
+            {
+                var specialTag = await _db.SpecialTags.FindAsync(id);
+
+                if (specialTag != null)
+                {
+                    return View(specialTag);
+                }
+                else
+                {
+                    return NotFound();
+                }
+
+
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, SpecialTags specialTags)
+        {
+            if (id == specialTags.Id)
+            {
+                if (ModelState.IsValid)
+                {
+                    _db.Update(specialTags);
+                    await _db.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+
+                }
+                else
+                {
+                    return NotFound();
+                }
+
+
+            }
+            else
+            {
+                return View(specialTags);
+
+            }
+
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                var specialTag = await _db.SpecialTags.FindAsync(id);
+                if (specialTag == null)
+                {
+                    return NotFound();
+
+                }
+                else
+                {
+                    return View(specialTag);
+                }
+
+            }
+
+
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                var specialTag = await _db.SpecialTags.FindAsync(id);
+                if (specialTag == null)
+                {
+                    return NotFound();
+
+                }
+                else
+                {
+                    return View(specialTag);
+                }
+
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(int id)
+        {
+           
+                var specialTag = await _db.SpecialTags.FindAsync(id);
+                 _db.Remove(specialTag);
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+
+
+
+            }
+          
+
+        
+
+
+
+
+
+
+
+    }
+}
