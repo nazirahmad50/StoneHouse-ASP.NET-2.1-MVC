4ca3530e9e522a44c855a695e3d6e08ca4afa29b
Nazir <n_ahmad20@live.co.uk>
2019-06-03 16:53:03 +0100

    AdminUsers Index page

---
+++ StoneHouse/Areas/Admin/Controllers/AdminUsersController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using StoneHouse.Data;
+
+namespace StoneHouse.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AdminUsersController : Controller
+    {
+
+        private readonly ApplicationDbContext _db;
+
+
+        public AdminUsersController(ApplicationDbContext db)
+        {
+            _db = db;
+
+        }
+
+        public IActionResult Index()
+        {
+            return View(_db.ApplicationUser.ToList());
+        }
+    }
+}
