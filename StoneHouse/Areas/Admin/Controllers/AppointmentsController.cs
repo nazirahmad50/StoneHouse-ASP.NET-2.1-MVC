86cd0db89024564078fa3647354e2a71da4c3656
Nazir <n_ahmad20@live.co.uk>
2019-06-05 11:49:43 +0100

    Add the sales person to the appointments table in teh database and create appointmetnsViewModel

---
+++ StoneHouse/Areas/Admin/Controllers/AppointmentsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using StoneHouse.Utility;
+
+namespace StoneHouse.Areas.Admin.Controllers
+{
+    //add multiple authorization roles to this controller
+    [Authorize(Roles = StaticDetails.AdminEndUser + "," + StaticDetails.SuperAdminEndUser)]
+    [Area("Admin")]
+    public class AppointmentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
