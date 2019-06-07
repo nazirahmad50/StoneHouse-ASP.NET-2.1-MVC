f91a2d1ae619429ddbbc127e96b49a2d09ccede5
Nazir <n_ahmad20@live.co.uk>
2019-06-07 16:12:08 +0100

    Page Link tag helper

---
+++ StoneHouse/TagHelpers/PageLinkTagHelper.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using StoneHouse.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StoneHouse.TagHelpers
+{
+    //the 'PageLinkTagHelper' will be assigned inside a div tag inside a view
+    //and add an attribute of page-model
+    [HtmlTargetElement("div", Attributes ="page-model")]
+    public class PageLinkTagHelper : TagHelper
+    {
+        //a factory for creating 'IUrlHelper' instance
+        private IUrlHelperFactory urlHelperFactory;
+
+        public PageLinkTagHelper(IUrlHelperFactory helperFactory)
+        {
+            urlHelperFactory = helperFactory;
+        }
+
+        //ViewContext provides access  to hhtp context, http request and others
+        [ViewContext]
+        //HtmlAttributeNotBound means this attribute isnt the one you intent to set via tag helper attribute in html
+        [HtmlAttributeNotBound]
+        public ViewContext ViewContext { get; set; }
+
+        //object of PagingInfo
+        public PagingInfo PageModel { get; set; }
+
+        public string PageAction { get; set; }
+        public bool PageClassesEnabled { get; set; }
+        public string PageClass { get; set; }
+        public string PageClassNormal { get; set; }
+        public string PageClassSelected { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            //fetch the url inside the url helper
+            IUrlHelper urlHelper = urlHelperFactory.GetUrlHelper(ViewContext);
+            //target element is div
+            TagBuilder result = new TagBuilder("div");
+
+            for (int i = 1; i <= PageModel.totalPage;i++)
+            {
+                TagBuilder tag = new TagBuilder("a");
+                //repalce the colon with 'i' in the url which is the pagination numbers
+                string url = PageModel.urlParam.Replace(":", i.ToString());
+                tag.Attributes["href"] = url;
+
+                if (PageClassesEnabled)
+                {
+                    tag.AddCssClass(PageClass);
+                    tag.AddCssClass(i == PageModel.CurrentPage ? PageClassSelected : PageClassNormal);
+
+                }
+                tag.InnerHtml.Append(i.ToString());
+                result.InnerHtml.AppendHtml(tag);
+            }
+            //append everything inside the div tag
+            //this is the parameter of this method
+            output.Content.AppendHtml(result.InnerHtml);
+        }
+
+
+    }
+}
