781a6bd1c5dc6aeb9b3fd00b05628c5d35574003
Nazir <n_ahmad20@live.co.uk>
2019-06-08 12:02:00 +0100

    Change the sites welcome heading from users email to users name through the use of View Componenets

---
+++ StoneHouse/ViewComponenets/UserNameViewComponent.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StoneHouse.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace StoneHouse.ViewComponents
+{
+    public class UserNameViewComponent : ViewComponent
+    {
+        private readonly ApplicationDbContext  _db;
+
+        public UserNameViewComponent(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            //get the current user 
+            var claimsIdentity = (ClaimsIdentity)this.User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            //get the current user from the database
+            var userFromDb = await _db.ApplicationUser.Where(u => u.Id == claim.Value).FirstOrDefaultAsync();
+
+            return View(userFromDb);
+        }
+    }
+}
